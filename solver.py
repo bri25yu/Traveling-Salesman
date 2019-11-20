5fbadd3de72feed6d2907d8ad64558f212f1ede8
Shadaj Laddad <shadaj@users.noreply.github.com>
2019-11-19 16:48:30 -0800

    Implement ILP-based solver that visits all homes

--- solver.py
+++ solver.py
@@ -1,9 +1,14 @@
+#!/usr/bin/env python3
+
 import os
 import sys
 sys.path.append('..')
 sys.path.append('../..')
 import argparse
 import utils
+import networkx as nx
+from mip import Model, xsum, minimize, BINARY, INTEGER
+from itertools import product
 
 from student_utils import *
 """
@@ -25,7 +30,96 @@ def solve(list_of_locations, list_of_homes, starting_car_location, adjacency_mat
         A dictionary mapping drop-off location to a list of homes of TAs that got off at that particular location
         NOTE: both outputs should be in terms of indices not the names of the locations themselves
     """
-    pass
+
+    location_name_to_index = {}
+    for i in range(0, len(list_of_locations)):
+        location_name_to_index[list_of_locations[i]] = i
+
+    home_indices = list(map(lambda home: location_name_to_index[home], list_of_homes))
+
+    starting_car_index = location_name_to_index[starting_car_location]
+
+    (G, message) = adjacency_matrix_to_graph(adjacency_matrix)
+    shortest_paths = nx.floyd_warshall(G)
+    L = range(0, len(list_of_locations))
+    nL = len(L)
+
+    model = Model()
+    edges_taken = [[model.add_var(var_type=BINARY) for j in L] for i in L]
+    visited_step = [model.add_var() for i in L]
+
+    model.objective = minimize(xsum(
+        G.edges[i, j]["weight"] * edges_taken[i][j] for i in L for j in L if G.has_edge(i, j)
+    ))
+
+    for i in L:
+        for j in L:
+            if not G.has_edge(i, j):
+                model += edges_taken[i][j] == 0
+
+    for city in [starting_car_index] + home_indices:
+        # enter city once
+        model += xsum(edges_taken[i][city] for i in set(L) - {city}) == 1
+        # leave city once
+        model += xsum(edges_taken[city][j] for j in set(L) - {city}) == 1
+
+    # enter and exit city once (this can be removed I think, but need to adjust path following logic)
+    for i in L:
+        model += xsum(edges_taken[incoming][i] for incoming in set(L) - {i}) <= 1
+        model += xsum(edges_taken[i][to] for to in set(L) - {i}) <= 1
+
+    # enter city same number of times as we exist the city
+    for i in L:
+        model += xsum(edges_taken[incoming][i] for incoming in set(L) - {i}) - xsum(edges_taken[i][to] for to in set(L) - {i}) == 0
+
+    # no subtours (Miller-Tucker-Zemlin formulation)
+    for (i, j) in set(product(set(L) - {0}, set(L) - {0})):
+        if i != j:
+            model += visited_step[i] - (nL + 1) * edges_taken[i][j] >= visited_step[j] - nL
+
+    for i in range(1, nL):
+        model += visited_step[i] >= 0
+        model += visited_step[i] <= nL - 1
+
+    status = model.optimize(max_seconds=300)
+    if model.num_solutions:
+        print("Edges taken:")
+        count = 0
+        for i in range(0, nL):
+            for j in range(0, nL):
+                if (edges_taken[i][j].x >= 0.99):
+                    print(i, j, G.edges[i, j]["weight"])
+                    count += 1
+        
+        print("Path:")
+        next_city = starting_car_index
+        other_count = 0
+        path = []
+        dropoffs = {}
+
+        while True:
+            print(next_city)
+            path.append(next_city)
+            next_city = [i for i in L if edges_taken[next_city][i].x >= 0.99][0]
+            other_count += 1
+            if next_city == starting_car_index:
+                print(next_city)
+                path.append(next_city)
+                break
+
+        for home in list_of_homes:
+            dropoffs[location_name_to_index[home]] = [location_name_to_index[home]]
+
+        # verify that we actually used all the edges we took, because I'm not 100% confident
+        # that the formulation works when we only need to visit a subset of nodes
+        if count != other_count:
+            print(count)
+            print(other_count)
+            raise RuntimeError("SOMETHING WRONG")
+        return (path, dropoffs)
+
+    return ([], {})
+
 
 """
 ======================================================================
